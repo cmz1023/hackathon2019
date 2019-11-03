288d9e1eb1f0083de77c957d586083478e0b3473
cmz1023 <cmz1023@gmail.com>
2019-11-02 22:58:29 -0400

    alot more functionality to GeicoWeitherAPI

--- src/GeicoWeatherAPI.java
+++ src/GeicoWeatherAPI.java
@@ -1,3 +1,8 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import jdk.nashorn.internal.parser.JSONParser;
 import kong.unirest.*;
 import kong.unirest.json.JSONObject;
@@ -9,9 +14,13 @@ import java.util.Vector;
 //^api key
 //text search: http://dataservice.accuweather.com/locations/v1/search
 //current conditions (GET): http://dataservice.accuweather.com/currentconditions/v1/07vMDheK0m6yvGnpXEpnqvkVZhyCFxo2
+
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.TwiMLException;
+
 public class GeicoWeatherAPI {
     public static void main(String[] args) throws Exception{
-
+        //sendTextMessage("+14129999653","hi mom i sent this from my hackpsu project don't respond to this number lol");
         Vector<String> weatherData = collectWeatherData("State College");
     }
     public static Vector<String>  collectWeatherData(String city){
@@ -30,6 +39,16 @@ public class GeicoWeatherAPI {
         vec.add(response.getBody().getArray().getJSONObject(0).get("LocalObservationDateTime").toString()); //Time weather was checked
         vec.add(response.getBody().getArray().getJSONObject(0).get("WeatherText").toString()); //Weather outside (clear, raining, etc..)
         vec.add(response.getBody().getArray().getJSONObject(0).getJSONObject("Temperature").getJSONObject("Imperial").get("Value").toString());
+        vec.add(response.getBody().getArray().getJSONObject(0).get("WeatherIcon").toString());
+        int num = new Integer(response.getBody().getArray().getJSONObject(0).get("WeatherIcon").toString());
+        Image image;
+        if (num < 10){
+            image = new Image("https://developer.accuweather.com/sites/default/files/0" + num + "-s.png");
+        }else{
+            image = new Image("https://developer.accuweather.com/sites/default/files/" + num + "-s.png");
+        }
+        ImageView imageView = new ImageView(image);
+        System.out.println("6");
         for (String x : vec){
             System.out.println(x);
         }
@@ -46,12 +65,12 @@ public class GeicoWeatherAPI {
         return 0;
     }
 
-    public static void sendTextMessage(String phoneNumber){
-
-    }
-
-    public static void sendEmail(String recipientEmail){
-
+    public static void sendTextMessage(String phoneNumber, String text){
+        String account_sid = "AC365c1ffc42023d5e0c12aad4332a7e03";
+        String account_auth = "5a6e893472adfb128416ab51838d6eb2";
+        Twilio.init(account_sid,account_auth);
+        Message message = Message.creator(new com.twilio.type.PhoneNumber(phoneNumber), new com.twilio.type.PhoneNumber("+12014821986"), text).create();
+        System.out.println(message.getSid());
     }
 
     public static void displayInformation(){
